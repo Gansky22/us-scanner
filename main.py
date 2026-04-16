3f3db71d0bc0064b2b9c33be7a8b3342f42728a5
Gansky22 <88711750+Gansky22@users.noreply.github.com>
2026-04-16 21:25:27 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -878,4 +878,5 @@ def route_run_scan_local():
 
 if __name__ == "__main__":
     setup_scheduler()
-    app.run(host="0.0.0.0", port=PORT)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
